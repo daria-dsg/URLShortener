16801444a0cf41b31a07094554b597103b4010eb
daria-dsg <daria.ui.ux@gmail.com>
2023-12-04 08:11:06 +0100

    add null constraint to email of users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_064902) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_070514) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
