cc2eb6e6d9f49cd473fc693022fbb576e767278e
daria-dsg <daria.ui.ux@gmail.com>
2023-12-05 11:14:39 +0100

    add method to create visit in db

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,4 +1,11 @@
 class Visit < ApplicationRecord
   validates :shortenedurl_id, presence: true
   validates :user_id, presence: true
+
+  def self.record_visit!(shortened_url, user)
+    Visit.create!(
+      shortenedurl_id: shortened_url.id,
+      user_id: user.id
+    )
+  end
 end
