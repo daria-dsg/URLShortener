d292b41ab562b5dae69f1eeb724509271aac3f70
daria-dsg <daria.ui.ux@gmail.com>
2023-12-11 06:48:10 +0100

    add function to return popular links

--- app/models/tag_topic.rb
+++ app/models/tag_topic.rb
@@ -7,4 +7,18 @@ class TagTopic < ApplicationRecord
     primary_key: :id,
     foreign_key: :tag_id
   )
+
+  has_many(
+    :urls,
+    through: :taggings,
+    source: :url
+  )
+
+  def popular_links
+    urls.joins(:visits)
+    .group(:short_url, :long_url)
+    .order('COUNT(visits.id) DESC')
+    .select('long_url, short_url, COUNT(visits.id) as number_of_visits')
+    .limit(5)
+  end
 end
